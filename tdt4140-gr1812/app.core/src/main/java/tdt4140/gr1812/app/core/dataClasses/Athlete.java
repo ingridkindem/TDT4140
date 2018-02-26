d4e0590783d9bfb24915ec570f6491ddc68a0dc9
marenbarth <maren_barth@hotmail.com>
2018-02-26 13:44:52 +0100

    Created Athlete class #31
    - Added attributes
    - Added constructor with phone number, first name and last name
    - Added getters and setters for each attribute
    - Created tests for all setters
    - Created tests for getters on firstName, lastName, fullName and phoneNumber

--- tdt4140-gr1812/app.core/src/main/java/tdt4140/gr1812/app/core/dataClasses/Athlete.java
+++ tdt4140-gr1812/app.core/src/main/java/tdt4140/gr1812/app/core/dataClasses/Athlete.java
@@ -1,5 +1,81 @@
 package tdt4140.gr1812.app.core.dataClasses;
 
 public class Athlete {
+	
+	private String phoneNumber; 
+	private String firstName; 
+	private String lastName; 
+	private int age;
+	private double weight;
+	private double height;
+	private int maxPulse;
+	private boolean gender; //true is female, false is male
+	//private List<Sport> sports = new ArrayList<Sport>();
+	
+	
+	public Athlete(String phoneNumber, String firstName, String lastName) {
+		this.phoneNumber = phoneNumber;
+		this.firstName = firstName; 
+		this.lastName = lastName;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public String getFullName() {
+		return firstName + " " + lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		if ((age < 0) || (age > 200)) {
+			throw new IllegalArgumentException("Age not valid.");
+		}
+		this.age = age;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public int getMaxPulse() {
+		return maxPulse;
+	}
+
+	public void setMaxPulse(int maxPulse) {
+		this.maxPulse = maxPulse;
+	}
+	
+	public boolean getGender() {
+		return gender;
+	}
+	
+	public void setGender(boolean gender) {
+		this.gender = gender;
+	}
 
 }
---
+++ tdt4140-gr1812/app.core/src/test/java/tdt4140/gr1812/app/core/AthleteTest.java
@@ -0,0 +1,96 @@
+package tdt4140.gr1812.app.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import tdt4140.gr1812.app.core.dataClasses.Athlete;
+
+public class AthleteTest {
+
+	private Athlete athlete;
+	
+	@Before
+	public void setUp() throws Exception {
+		 this.athlete = new Athlete("95902393", "Maren" , "Barth");
+	}
+	
+	@Test
+	public void testGetPhoneNumber() {
+		assertEquals("95902393", athlete.getPhoneNumber());
+	}
+		
+	
+	@Test
+	public void testGetFirstName() {
+		assertEquals("Maren", athlete.getFirstName());
+	}
+	
+	@Test
+	public void testGetLastName() {
+		assertEquals("Barth", athlete.getLastName());
+	}
+	
+	@Test
+	public void testGetFullName() {
+		assertEquals("Maren Barth", athlete.getFullName());
+	}
+	
+	
+	@Test
+	public void testSetAge() {
+		athlete.setAge(25);
+		assertEquals(25, athlete.getAge());
+	}
+	
+	@Test
+	public void testSetAgeThrowsIllegalArgumentException() {
+		try {
+	        athlete.setAge(201);
+	        System.out.println("Should have thrown IllegalArgumentException.");
+	        assert false;
+	    } catch (IllegalArgumentException e) {
+	        assert true;
+	    }
+		try {
+	        athlete.setAge(-1);
+	        System.out.println("Should have thrown IllegalArgumentException.");
+	        assert false;
+	    } catch (IllegalArgumentException e) {
+	        assert true;
+	    }
+	}
+	
+	@Test
+	public void testSetWeight() {
+		athlete.setWeight(60);
+		assertTrue(60 == athlete.getWeight());
+	}
+	
+	@Test
+	public void testSetGender() {
+		athlete.setGender(true);
+		assertTrue(athlete.getGender());
+		athlete.setGender(false);
+		assertFalse(athlete.getGender());
+	}
+	
+	@Test
+	public void testSetHeight() {
+		athlete.setHeight(170.3);
+		assertTrue(170.3 == athlete.getHeight());
+	}
+	
+	@Test
+	public void testSetMaxPulse() {
+		athlete.setMaxPulse(230);
+		assertTrue(230 == athlete.getMaxPulse());
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+}
