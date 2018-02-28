c8623dff6f922ffd643a6347157bb7ac0345e671
Toralf <toralffrich@gmail.com>
2018-02-28 13:09:02 +0100

    initialCommit

--- tdt4140-gr1812/app.core/src/test/java/tdt4140/gr1812/app/core/ExampleTest.java
+++ tdt4140-gr1812/app.core/src/test/java/tdt4140/gr1812/app/core/ExampleTest.java
@@ -2,13 +2,21 @@ package tdt4140.gr1812.app.core;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.Test;
+
+import tdt4140.gr1812.app.core.model.SignUpModel;
 
 public class ExampleTest {
 
 	@Test
 	public void TrueTest() {
-		assertTrue(true);
+		SignUpModel model = new SignUpModel();
+		model.setcellPhoneNumber(1234);
+		System.out.print(model.getcellPhoneNumber());
+		//assertTrue(model.getcellPhoneNumber() == 12345); 
+		
+		//assertTrue(true);
 	}
 
 }
