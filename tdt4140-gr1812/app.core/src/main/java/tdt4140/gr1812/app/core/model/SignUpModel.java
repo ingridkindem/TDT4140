723604596df27658ecc20b8e0706843e10dc234d
Toralf <toralffrich@gmail.com>
2018-02-28 16:06:17 +0100

    #30 Almost Finished
    Still need to connect to Backend.

--- tdt4140-gr1812/app.core/src/main/java/tdt4140/gr1812/app/core/model/SignUpModel.java
+++ tdt4140-gr1812/app.core/src/main/java/tdt4140/gr1812/app/core/model/SignUpModel.java
@@ -5,72 +5,92 @@ public class SignUpModel {
 	private int cellPhoneNumber;
 	private String firstName;
 	private String surName;
-	private int height;
+	private String passWord;
 	private int maxPulse;
 	private int weight;
 	// private List<Sport> Idretter = new List<Sport>;
-	
-	/*
-	 * 	this.firstName = getfirstName();
-		this.surName = getsurName();
-		this.height = getheight();
-		this.maxPulse = getmaxPulse();
-		this.weight = getweight();
-	 */
-	
+
 	public SignUpModel() {
 		this.cellPhoneNumber = getcellPhoneNumber();
+		this.firstName = getfirstName();
+        this.surName = getsurName();
+        this.passWord = getPassWord();
+        this.maxPulse = getmaxPulse();
+        this.weight = getweight();
 	}
 	
 	public int getcellPhoneNumber() {
 		return cellPhoneNumber;
 	}
-	public void setcellPhoneNumber(int cellPhoneNumber) {
-		try{
-			if (String.valueOf(cellPhoneNumber).length() > 8) {
-				throw new IllegalArgumentException("Du kan ikke ha et tall som er lengre enn 8 tall.");
-			}
-			this.cellPhoneNumber = cellPhoneNumber;
-		} catch (IllegalArgumentException e){
-			System.out.println("Du må skrive inn et tall");
+	public void setcellPhoneNumber(String cellPhoneNumber) {
+		if (cellPhoneNumber.length() != 8) {
+			throw new IllegalArgumentException("Et telefonnummer må være 8 sifre langt.");
+		}
+		try
+		{
+		    this.cellPhoneNumber = Integer.parseInt(cellPhoneNumber);
+		}
+		catch (NumberFormatException nfe)
+		{
+		   throw new IllegalArgumentException("Could not convert from string to int."); 
 		}
 	}
+		
 	public String getfirstName() {
 		return firstName;
 	}
+	
 	public void setfirstName(String firstName) {
+		if (!firstName.matches("[a-åA-Å]+$")) {
+			throw new IllegalArgumentException("Du kan ikke skrive inn tall i et navn.");
+		} else if (firstName == ""){
+			throw new IllegalArgumentException("Du kan ikke ikke ha noe navn");
+		}
 		this.firstName = firstName;
 	}
+	
 	public String getsurName() {
 		return surName;
 	}
+	
 	public void setsurName(String surName) {
-		this.surName = surName;
+	    if (!surName.matches("[a-åA-Å]+$")) {
+            throw new IllegalArgumentException("Du kan ikke skrive inn tall i et navn.");
+        } else if (surName == ""){
+            throw new IllegalArgumentException("Du kan ikke ikke ha noe navn");
+        }
+        this.surName = surName;
+    }
+	
+	
+	public String getPassWord() {
+		return this.passWord;
 	}
-	public int getheight() {
-		return height;
-	}
-	public void setheight(int height) {
-		this.height = height;
+	public void setPassWord(String password) {
+	    if (password == ""){
+            throw new IllegalArgumentException("Du kan ikke ikke ha noe navn");
+        }
+		this.passWord = password;
 	}
 	public int getmaxPulse() {
 		return maxPulse;
 	}
 	public void setmaxPulse(int maxPulse) {
+	    if (maxPulse < 0) {
+	        throw new IllegalArgumentException("Du kan ikke ha en negativ puls!");
+	    }
 		this.maxPulse = maxPulse;
 	}
+	
 	public int getweight() {
 		return weight;
 	}
 	public void setweight(int weight) {
-		this.weight = weight;
-	}
-	
-//	public static void main(String[] args) {
-//		SignUpModel toralf = new SignUpModel();
-//		toralf.setcellPhoneNumber(12345678);
-//		System.out.println(toralf.cellPhoneNumber);
-//	}
+	    if (weight < 0) {
+            throw new IllegalArgumentException("Du kan ikke ha en negativ puls!");
+        }
+        this.weight = weight;
+    }
 
 }
 
--- tdt4140-gr1812/app.core/src/test/java/tdt4140/gr1812/app/core/SignUpModelTest.java
+++ tdt4140-gr1812/app.core/src/test/java/tdt4140/gr1812/app/core/SignUpModelTest.java
@@ -8,23 +8,185 @@ import org.junit.Test;
 import tdt4140.gr1812.app.core.model.SignUpModel;
 
 //HUSK Å LEGGE INN SPORT KLASSEN
+//Hvilke tester skal du kjøre? 
+//Test at alle Exceptions kjører som de skal
+//Test at alle Getters og Setters kjører som de skal.
 
 public class SignUpModelTest {
 	
-	SignUpModel model;
-	
-	SignUpModel test = new SignUpModel();
+	@Test
+	public void testSetCellPhoneNumberLengthLong() {
+	    SignUpModel model = new SignUpModel();
+	    try {
+	        model.setcellPhoneNumber("123456789");
+	    }
+	    catch (IllegalArgumentException e){
+	        assertTrue(true);
+	    }
+	}
 	
 	@Test
-	public void testSetCellPhoneNumber() {
-		test.setcellPhoneNumber(1234);
-		assertTrue(test.getcellPhoneNumber() == 1234);
-	}
-
+    public void testSetCellPhoneNumberLengthShort() {
+        SignUpModel model = new SignUpModel();
+        try {
+            model.setcellPhoneNumber("1234567");
+        }
+        catch (IllegalArgumentException e){
+            assertTrue(true);
+        }
+    }
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+    public void testSetCellPhoneNumberWithString() {
+        SignUpModel model = new SignUpModel();
+        try {
+            model.setcellPhoneNumber("abc");
+        }
+        catch (IllegalArgumentException e){
+            assertTrue(true);
+        }
+    }
+	@Test
+	public void testSetCellPhoneNumberSetterValid() {
+	    SignUpModel model = new SignUpModel();
+	     try {
+	        model.setcellPhoneNumber("12345678");
+	        assertTrue(model.getcellPhoneNumber() == 12345678);
+	     }
+	     catch (IllegalArgumentException e){
+	        assertTrue(true);
+	     }
+	 }
+	
+	
+	@Test
+	public void testsetfirstNameTall() {
+	    SignUpModel model = new SignUpModel();
+	    try{
+	        model.setfirstName("1234");
+	    } catch (IllegalArgumentException e) {
+	        assertTrue(true);
+	    }
 	}
+	@Test
+    public void testsetfirstNameEmptyString() {
+        SignUpModel model = new SignUpModel();
+        try{
+            model.setfirstName("");
+        } catch (IllegalArgumentException e) {
+            assertTrue(true);
+        }
+    }
+	
+	@Test
+	public void testsetfirstNameValid() {
+	    SignUpModel model = new SignUpModel();
+	    try {
+	        model.setfirstName("Toralf");
+	        assertTrue(model.getfirstName() == "Toralf");
+	    } catch (IllegalArgumentException e){
+            assertTrue(true);
+        }
+	}
+	
+	@Test
+    public void testsetsurNameTall() {
+        SignUpModel model = new SignUpModel();
+        try{
+            model.setfirstName("1234");
+        } catch (IllegalArgumentException e) {
+            assertTrue(true);
+        }
+    }
+	
+    @Test
+    public void testsetsurNameEmptyString() {
+        SignUpModel model = new SignUpModel();
+        try{
+            model.setfirstName("");
+        } catch (IllegalArgumentException e) {
+            assertTrue(true);
+        }
+    }
+    
+    @Test
+    public void testsetsurNameValid() {
+        SignUpModel model = new SignUpModel();
+        try {
+            model.setfirstName("Toralf");
+            assertTrue(model.getfirstName() == "Toralf");
+        } catch (IllegalArgumentException e){
+            assertTrue(true);
+        }
+    }
+    
+    @Test
+    public void testPassWordValid() {
+        SignUpModel model = new SignUpModel();
+        try {
+            model.setPassWord("Toralf");
+            assertTrue(model.getPassWord() == "Toralf");
+        } catch (IllegalArgumentException e){
+            assertTrue(true);
+        }
+    }
+    
+    @Test
+    public void testsetPassWordEmptyString() {
+        SignUpModel model = new SignUpModel();
+        try{
+            model.setPassWord("");
+        } catch (IllegalArgumentException e) {
+            assertTrue(true);
+        }
+    }
+    
+    @Test
+    public void testsetMaxPulseNegative() {
+        SignUpModel model = new SignUpModel();
+        try{
+            model.setmaxPulse(-190);
+        } catch (IllegalArgumentException e) {
+            assertTrue(true);
+        }
+    }
+    
+    @Test
+    public void testsetMaxPulseValid() {
+        SignUpModel model = new SignUpModel();
+        try {
+            model.setmaxPulse(200);
+            assertTrue(model.getmaxPulse() == 200);
+        } catch (IllegalArgumentException e){
+            assertTrue(true);
+        }
+    }
+    
+    @Test
+    public void testsetWeightNegative() {
+        SignUpModel model = new SignUpModel();
+        try{
+            model.setweight(-190);
+        } catch (IllegalArgumentException e) {
+            assertTrue(true);
+        }
+    }
+    
+    @Test
+    public void testsetWeightValid() {
+        SignUpModel model = new SignUpModel();
+        try {
+            model.setmaxPulse(200);
+            assertTrue(model.getmaxPulse() == 200);
+        } catch (IllegalArgumentException e){
+            assertTrue(true);
+        }
+    }
+    
+    
+    
+	
+	
 	
 	/*
 	 * model.setfirstName("Toralf");
