804cbde9508f41d291e69fbd1387eb73b7b6eb36
Toralf <toralffrich@gmail.com>
2018-02-28 13:09:02 +0100

    initialCommit

---
+++ tdt4140-gr1812/app.core/src/test/java/tdt4140/gr1812/app/core/SignUpModelTest.java
@@ -0,0 +1,35 @@
+package tdt4140.gr1812.app.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import tdt4140.gr1812.app.core.model.SignUpModel;
+
+//HUSK Å LEGGE INN SPORT KLASSEN
+
+public class SignUpModelTest {
+	
+	SignUpModel model;
+	
+	SignUpModel test = new SignUpModel();
+	
+	@Test
+	public void testSetCellPhoneNumber() {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * model.setfirstName("Toralf");
+		model.setsurName("Frich");
+		model.setheight(202);
+		model.setweight(100);
+		model.setmaxPulse(200);
+	 */
+
+}
