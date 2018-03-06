4568992b509ac20269530cf7df84e4c233766e18
Collatty <hakoncb@gmail.com>
2018-03-06 23:26:14 +0100

    Server class #45
    Running with Spring

---
+++ backend/complete/src/main/java/tdt4140/gr1812/app/backend/server/Server.java
@@ -0,0 +1,21 @@
+package tdt4140.gr1812.app.backend.server;
+
+import java.util.Arrays;
+
+//import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Server {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Server.class, args);
+    }
+   
+}
+
+
+
