73018b52706fa57ce4aa4716429c6852e3fc801c
Caroline Lysebo <caroline@macama.no>
2018-03-08 14:02:48 +0100

    Created WorkoutRegistrationController with test #69
    Created the base for the WorkoutRegistrationController and the test. Changed the fields in the WorkoutRegistrationView to match the controller. Fixed issue with image in the view.

---
+++ tdt4140-gr1812/app.ui/src/test/java/tdt4140/gr1812/app/ui/controllers/WorkoutRegistrationControllerTest.java
@@ -0,0 +1,21 @@
+package tdt4140.gr1812.app.ui.controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WorkoutRegistrationControllerTest {
+	
+	private WorkoutRegistrationController controller;
+
+	@Test
+	public void testHandleRegistrer() {
+		
+	}
+	
+	@Test
+	public void testHandleKryssUt() {
+		
+	}
+
+}
