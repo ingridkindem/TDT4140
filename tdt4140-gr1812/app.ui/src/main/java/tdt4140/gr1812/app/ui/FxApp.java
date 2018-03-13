353116a0d333c92a4e1db7aa433f51e72c890a48
Sigrid Schaanning <s-schaa@online.no>
2018-03-13 12:40:04 +0100

    WorkoutClass Issue#34
    In this class we have created several methods to register information about a work out. This includes duration, distance, pulses, privacy, calories, the date and comments about the work out.

--- tdt4140-gr1812/app.ui/src/main/java/tdt4140/gr1812/app/ui/FxApp.java
+++ tdt4140-gr1812/app.ui/src/main/java/tdt4140/gr1812/app/ui/FxApp.java
@@ -1,22 +1,12 @@
 package tdt4140.gr1812.app.ui;
 
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import tdt4140.gr1812.app.ui.controllers.LoginController;
-import tdt4140.gr1812.app.ui.controllers.SignupController;
-
 
 public class FxApp extends Application {
-	
-	private FXMLLoader fxmlLoader;
-	private Stage stage;
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -83,6 +73,7 @@ public class FxApp extends Application {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+
     }
 
     public static void main(String[] args) {
