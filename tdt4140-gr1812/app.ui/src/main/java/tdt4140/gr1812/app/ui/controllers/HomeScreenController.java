092f061043adc89da876860be9d7214df1fa50c1
ingriak <ingriak@stud.ntnu.no>
2018-03-26 12:34:50 +0200

    Created goToHome(), changed some views, fixed onAction in fxml
    LoginController now contains goToHome(), a method for switching to
    homeScreen. Changed both loginView and homeScreenView; login now
    contains homescreen button, and homeScreen now has buttons for
    Facebook, contact and maps. Deleted FxAppTest file, runs OK on FxApp
    now. HomeScreenController now contains methods for handling facebook,
    mail, map and going back to login.

--- tdt4140-gr1812/app.ui/src/main/java/tdt4140/gr1812/app/ui/controllers/HomeScreenController.java
+++ tdt4140-gr1812/app.ui/src/main/java/tdt4140/gr1812/app/ui/controllers/HomeScreenController.java
@@ -2,28 +2,17 @@ package tdt4140.gr1812.app.ui.controllers;
 
 
 import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 
 import javafx.scene.control.Hyperlink;
-import javafx.scene.layout.StackPane;
 import javafx.scene.web.WebView;
 import tdt4140.gr1812.app.ui.FxApp;
 
 public class HomeScreenController {
 
     @FXML
-    private Hyperlink facebookB;
-
-    @FXML
-    private Hyperlink mailB;
-
-    @FXML
-    private Hyperlink phoneB;
-
-    @FXML
-    private Hyperlink lierB;
+    private Hyperlink facebookB, mailB, phoneB, lierB;
 
     @FXML
     private WebView maps;
@@ -31,24 +20,14 @@ public class HomeScreenController {
     @FXML
     private FxApp app;
 
-
-
-    @FXML
-    private ResourceBundle resources;
-
     @FXML
     private URL location;
-
-    @FXML
-    private StackPane root;
 
 
     @FXML
     public void initialize() {
-        
        maps.getEngine().load("file:///Users/bruker/tdt4180-oxygen/12/tdt4140-gr1812/app.ui/src/main/java/tdt4140/gr1812/app/ui/views/homeScreen/location.html");
     }
-
 
     public void setApplication(FxApp app) {
         this.app = app;
@@ -58,7 +37,26 @@ public class HomeScreenController {
         Application.launch(args);
     }
 
+    @FXML
+    public void handleFacebook() {
+        maps.getEngine().load("https://www.facebook.com/lierkvinner/");
 
+    }
+    
+    @FXML
+    public void handleMail() {
+        maps.getEngine().load("https://lieridrettslag.weborg.no/Lier-IL/Toppmeny/Kontakt-oss.html#/Hovedstyret");
+    }
+    
+    public void handleMap() {
+        maps.getEngine().load("file:///Users/bruker/tdt4180-oxygen/12/tdt4140-gr1812/app.ui/src/main/java/tdt4140/gr1812/app/ui/views/homeScreen/location.html");
+
+    }
+
+    @FXML
+    public void handleBack() {
+        app.goToLogin();
+    }
 }
 
 
