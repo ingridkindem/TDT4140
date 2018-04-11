0df373c867d6cd2fb4f0f3a020f854e4aa6003fa
Collatty <hakoncb@gmail.com>
2018-04-11 16:51:29 +0200

    Added test for login #104 WIP

--- backend/src/test/java/tdt4140/gr1812/app/backend/ServerTest.java
+++ backend/src/test/java/tdt4140/gr1812/app/backend/ServerTest.java
@@ -1,9 +1,12 @@
 package tdt4140.gr1812.app.backend;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.containsString;
 
+import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +18,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import tdt4140.gr1812.app.backend.server.Server;
+import tdt4140.gr1812.app.backend.server.ServerController;
+import tdt4140.gr1812.app.backend.server.ServerLogic;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes= Server.class)
@@ -26,9 +31,36 @@ public class ServerTest{
 
     @Test //Testing that endpoint responds properly
     public void welcomeMessage() throws Exception {
+    	
         mvc.perform(MockMvcRequestBuilders.get("").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("Welcome. I am your server.")));
+        
+        
     }
-    //Have to figure out how to test for login and signup
+    
+    @Test
+    public void TestLogin() throws Exception{
+    	
+    	String username = "12345678";
+    	String password = "12345678";
+    	
+    				mvc.perform(MockMvcRequestBuilders.get("/login")
+    				.param("username", username)
+    				.param("password", password))
+    				.andExpect(status().isOk())
+    				//.andExpect(content().contentType("application/json"))
+    	            .andExpect(content().string(containsString("success")));
+    				
+    				
+    		
+    				
+    				
+    		
+    		
+    }
+    
+    
+    
+    
 }
