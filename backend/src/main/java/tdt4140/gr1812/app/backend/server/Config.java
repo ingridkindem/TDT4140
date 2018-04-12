88c1e61b2ecb5c18103a2bd0aa64df0d3b4c761e
Collatty <hakoncb@gmail.com>
2018-04-12 13:56:58 +0200

    Inserted correct info #104

--- backend/src/main/java/tdt4140/gr1812/app/backend/server/Config.java
+++ backend/src/main/java/tdt4140/gr1812/app/backend/server/Config.java
@@ -4,9 +4,9 @@ package tdt4140.gr1812.app.backend.server;
  * Created by LarsErik on 12/04/2018.
  */
 public class Config {
-    static String dbUser = "root";
-    static String dbPass = "root";
-    static String dbHost = "localhost";
-    static String dbName = "pu";
+    static String dbUser = "pu";
+    static String dbPass = "ZAmrUsPWD3vd";
+    static String dbHost = "larserikfagernaes.com";
+    static String dbName = "PU";
     static int dbPort = 3306;
 }
