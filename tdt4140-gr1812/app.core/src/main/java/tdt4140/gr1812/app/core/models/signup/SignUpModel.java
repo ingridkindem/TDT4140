eb663339e9b5af79dc0b29095902fd84dff78163
marenbarth <maren_barth@hotmail.com>
2018-04-13 13:05:02 +0200

    Fixed the Signup #85 #114
    -Changed the "idrett"-drop down menu to look nice with css and as the "idrett"-drop down on "register workout". Now the design is the same in all windows.
    - Change to the type "PasswordField" where user choose passord, so you can't see the password the user chooses.
    - Created feedback text when trying to register a user with invalid data in phone number, first name, last name, max pulse and weight. Also created feedback if a user doesn't select a sport.
    - Created tests for all the check-methods, and fixed the previous tests for the signup model.
    - Removed all of the setters and getters in the signup model, and made the methods static. There is no need for the setters and getters.

--- tdt4140-gr1812/app.core/src/main/java/tdt4140/gr1812/app/core/models/signup/SignUpModel.java
+++ tdt4140-gr1812/app.core/src/main/java/tdt4140/gr1812/app/core/models/signup/SignUpModel.java
@@ -2,60 +2,35 @@ package tdt4140.gr1812.app.core.models.signup;
 
 import java.util.HashMap;
 import org.json.JSONObject;
+
+import tdt4140.gr1812.app.core.dataClasses.Sport;
 import tdt4140.gr1812.app.core.helpers.BackendConnector;
 import tdt4140.gr1812.app.core.helpers.Gender;
 import tdt4140.gr1812.app.core.helpers.Method;
 
 public class SignUpModel {
 	
-	private int cellPhoneNumber;
-	private String firstName;
-	private String surName;
-	private String passWord;
-	private int maxPulse;
-	private int weight;
-	// private List<Sport> Idretter = new List<Sport>;
 
-	public SignUpModel() {
- 
-		this.cellPhoneNumber = getcellPhoneNumber();
-		this.firstName = getfirstName();
-        this.surName = getsurName();
-        this.passWord = getPassWord();
-        this.maxPulse = getmaxPulse();
-        this.weight = getweight();
-	}
+	private static String text;
 	
-	public int getcellPhoneNumber() {
-		return cellPhoneNumber;
-	}
-	public void setcellPhoneNumber(String cellPhoneNumber) {
-		if (cellPhoneNumber.length() != 8) {
-			throw new IllegalArgumentException("Et telefonnummer må være 8 sifre langt.");
-		}
-		try
-		{
-		    this.cellPhoneNumber = Integer.parseInt(cellPhoneNumber);
-		}
-		catch (NumberFormatException nfe)
-		{
-		   throw new IllegalArgumentException("Could not convert from string to int."); 
-		}
-	}
-		
-	public String getfirstName() {
-		return firstName;
-	}
 	
-	public boolean signupUser(int phonenumber, String password, String sport, String firstName, String surname, int maxpulse, int weight, Gender gender) {
+	//Method to sign up a user, with backend connection
+	public static boolean signupUser(int phonenumber, String password, String sport, String firstName, String surname, int maxpulse, int weight, Gender gender) {
 	       try {
-	           
+	    	   text = "";
+	    	   checkFirstName(firstName);
+	    	   checkLastName(surname);
+	    	   checkPhoneNumber(String.valueOf(phonenumber));
+	    	   checkSport(sport);
+	    	   checkWeight(String.valueOf(weight));
+	    	   checkMaxPulse(String.valueOf(maxpulse));
+	  
 	           String genderString = gender == gender.FEMALE ? "female" : "male";  
 	           
 	           HashMap requestParam = new HashMap<String, String>();
 	           requestParam.put("username", String.valueOf(phonenumber));
 	           requestParam.put("password", password);
-	           requestParam.put("sport", sport);
+	           requestParam.put("sport", sport); 
 	           requestParam.put("firstname", firstName);
 	           requestParam.put("surname", surname);
 	           requestParam.put("maxpulse", String.valueOf(maxpulse));
@@ -76,54 +51,84 @@ public class SignUpModel {
 	       }
 	}
 	
-	public void setfirstName(String firstName) {
-		if (!firstName.matches("[a-åA-Å]+$")) {
-			throw new IllegalArgumentException("Du kan ikke skrive inn tall i et navn.");
-		} else if (firstName == ""){
+	//Method to check if the first name is valid
+	public static void checkFirstName(String firstName) {
+		firstName.toLowerCase();
+		char[] chars = firstName.toCharArray();
+
+	    for (char c : chars) {
+	        
+	    	if(!Character.isLetter(c)) {
+	        	text = "Navnet kan kun inneholde bokstaver.";
+	        	throw new IllegalArgumentException("Du kan ikke skrive inn tall i et navn.");
+	        }
+	    }
+		if (firstName == ""){
+			text = "Du m� skrive inn et navn.";
 			throw new IllegalArgumentException("Du kan ikke ikke ha noe navn");
 		}
-		this.firstName = firstName;
 	}
 	
-	public String getsurName() {
-		return surName;
-	}
-	
-	public void setsurName(String surName) {
-	    if (!surName.matches("[a-åA-Å]+$")) {
-            throw new IllegalArgumentException("Du kan ikke skrive inn tall i et navn.");
-        } else if (surName == ""){
-            throw new IllegalArgumentException("Du kan ikke ikke ha noe navn");
-        }
-        this.surName = surName;
-    }
-	
-	public String getPassWord() {
-		return this.passWord;
-	}
-	public void setPassWord(String password) {
-	    if (password == ""){
-            throw new IllegalArgumentException("Du kan ikke ikke ha noe navn");
-        }
-		this.passWord = password;
-	}
-	public int getmaxPulse() {
-		return maxPulse;
-	}
-	public void setmaxPulse(int maxPulse) {
-	    if (maxPulse < 0) {
-	        throw new IllegalArgumentException("Du kan ikke ha en negativ puls!");
+	//Method to check if the last name is valid
+	public static void checkLastName(String surName) {
+		surName.toLowerCase();
+		char[] chars = surName.toCharArray();
+
+	    for (char c : chars) {
+	        
+	    	if(!Character.isLetter(c)) {
+	        	text = "Navnet kan kun inneholde bokstaver.";
+	        	throw new IllegalArgumentException("Du kan ikke skrive inn tall i et navn.");
+	        }
 	    }
-		this.maxPulse = maxPulse;
+		if (surName == ""){
+			text = "Du m� skrive inn et navn.";
+			throw new IllegalArgumentException("Du kan ikke ikke ha noe navn");
+		}
+	}
+
+	//Method to check if the phone number is valid
+	public static void checkPhoneNumber(String number) {
+		if (number.length() != 8) {
+			text = "Phone number must be 8 numbers.";
+			throw new IllegalArgumentException("Et telefonnummer maa vaere 8 sifre langt.");
+		}
+		for (int i = 0; i < number.length(); i++){
+		   char c = number.charAt(i);
+		   if (!Character.isDigit(c)){
+			   text = "Phone number can only contain numbers.";
+			   throw new IllegalArgumentException("Could not convert from string to int."); 
+		   }
+		}
 	}
 	
-	public int getweight() {
-		return weight;
+	//method to check if no sport is selected
+	public static void checkSport(String sport) {
+		if (sport == "") {
+			text = "Sport field is empty.";
+			throw new IllegalArgumentException("Sport field is empty");
+		}
 	}
-	public void setweight(int weight) {
-	    if (weight < 0) {
-            throw new IllegalArgumentException("Du kan ikke ha en negativ puls!");
-        }
-        this.weight = weight;
-    }
+	
+//method to check if the max pulse is valid
+	public static void checkMaxPulse(String maxPulse) {
+		if (Integer.parseInt(maxPulse) < 0) {
+			text = "Can't have negative max pulse.";
+			throw new IllegalArgumentException("Can't have negative max pulse.");
+		}
+	}
+	
+	//method to check if the max pulse is valid
+	public static void checkWeight(String weight) {
+		if (Integer.parseInt(weight) < 0) {
+			text = "Can't have negative weight.";
+			throw new IllegalArgumentException("Can't have negative weight.");
+		}
+	}
+	
+	//returns text on the feedback field
+	public String getText() { 
+		return text;
+	}
+		
 }
